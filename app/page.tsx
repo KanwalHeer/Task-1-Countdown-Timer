a983a677acd2b716e4ec44429b67529f35ddff65
ali langah <alilangah78@gmail.com>
2024-12-09 14:32:39 +0500

    comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 
-  import Countdown from "./counter/counter"
-  
+  import HomeBanner from "@/components/pageComponents/home/homeBanner"
+  import HomeItems from "@/components/pageComponents/home/homeItems"
+  import HomeProduct from "@/components/pageComponents/home/homeProduct"
+  import ExploreSectio from "@/components/pageComponents/home/exploreSectio"
+  import ImageDisplay from "@/components/pageComponents/home/imageDisplay"
   export default function page() {
     return (
       <div>
-        <Countdown/>
+        <HomeBanner/>
+        <HomeItems/>
+        <HomeProduct/>
+        <ExploreSectio/>
+        <ImageDisplay/>
       </div>
     )
   }
